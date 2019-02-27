71389d3832ca52b53e4996358b807b837e6c12c9
Dunatron <heath.dunlop.hd@gmail.com>
2019-02-28 00:53:17 +1300

    ff

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,76 +1,18 @@
-// const cookieParser = require("cookie-parser")
-// const jwt = require("jsonwebtoken")
-
-// require("dotenv").config({ path: "./variables.env" })
-// const createServer = require("./createServer")
-// const db = require("./db")
-// const { runAllJobs } = require("./cronjobs")
-// // const { tronCrawler } = require("./lib/websiteCrawler")
-
-// // run all our cron jobs
-// runAllJobs()
-// // const sitesToCrawl = ["https://www.op.ac.nz"]
-// // const crawlableDomains = ["www.op.ac.nz", "central.op.ac.nz"]
-// // tronCrawler(sitesToCrawl, crawlableDomains)
-
-// const server = createServer()
-
-// const expressLogger = function(req, res, next) {
-//   console.log("express endpoint called")
-//   next()
-// }
-
-// server.use(expressLogger)
-
-// server.express.use(cookieParser())
-
-// // decode the JWT so we can get the user Id on each request
-// server.express.use((req, res, next) => {
-//   const { token } = req.cookies
-//   console.log("token => ", token)
-//   if (token) {
-//     const { userId } = jwt.verify(token, process.env.APP_SECRET)
-//     // put the userId onto the req for future requests to access
-//     req.userId = userId
-//   }
-//   next()
-// })
-
-// // 2. Create a middleware that populates the user on each request
-// server.express.use(async (req, res, next) => {
-//   // if they aren't logged in, skip this
-//   if (!req.userId) return next()
-//   const user = await db.query.user(
-//     { where: { id: req.userId } },
-//     "{ id, permissions, email, name }"
-//   )
-//   req.user = user
-//   next()
-// })
-
-// server.get("/tron-search", function(req, res) {
-//   var searchFile = require("../cronjob-files/pages.json")
-//   res.send(searchFile)
-// })
-
-// server.start(
-//   {
-//     cors: {
-//       credentials: true,
-//       origin: process.env.FRONTEND_URL,
-//     },
-//   },
-//   details => {
-//     console.log(`Server is now running on port http:/localhost:${details.port}`)
-//   }
-// )
 const cookieParser = require("cookie-parser")
 const jwt = require("jsonwebtoken")
 
 require("dotenv").config({ path: "./variables.env" })
-
 const createServer = require("./createServer")
 const db = require("./db")
+const { runAllJobs } = require("./cronjobs")
+// const { tronCrawler } = require("./lib/websiteCrawler")
+
+// run all our cron jobs
+runAllJobs()
+// const sitesToCrawl = ["https://www.op.ac.nz"]
+// const crawlableDomains = ["www.op.ac.nz", "central.op.ac.nz"]
+// tronCrawler(sitesToCrawl, crawlableDomains)
+
 const server = createServer()
 
 const expressLogger = function(req, res, next) {
@@ -80,12 +22,12 @@ const expressLogger = function(req, res, next) {
 
 server.use(expressLogger)
 
-// use express middleware to handle cookies (JWT)
 server.express.use(cookieParser())
 
-// 1. decode the JWT so we can get the user Id on each request
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies
+  console.log("token => ", token)
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET)
     // put the userId onto the req for future requests to access
@@ -106,6 +48,11 @@ server.express.use(async (req, res, next) => {
   next()
 })
 
+server.get("/tron-search", function(req, res) {
+  var searchFile = require("../cronjob-files/pages.json")
+  res.send(searchFile)
+})
+
 server.start(
   {
     cors: {
@@ -117,3 +64,56 @@ server.start(
     console.log(`Server is now running on port http:/localhost:${details.port}`)
   }
 )
+// const cookieParser = require("cookie-parser")
+// const jwt = require("jsonwebtoken")
+
+// require("dotenv").config({ path: "./variables.env" })
+
+// const createServer = require("./createServer")
+// const db = require("./db")
+// const server = createServer()
+
+// const expressLogger = function(req, res, next) {
+//   console.log("express endpoint called")
+//   next()
+// }
+
+// server.use(expressLogger)
+
+// // use express middleware to handle cookies (JWT)
+// server.express.use(cookieParser())
+
+// // 1. decode the JWT so we can get the user Id on each request
+// server.express.use((req, res, next) => {
+//   const { token } = req.cookies
+//   if (token) {
+//     const { userId } = jwt.verify(token, process.env.APP_SECRET)
+//     // put the userId onto the req for future requests to access
+//     req.userId = userId
+//   }
+//   next()
+// })
+
+// // 2. Create a middleware that populates the user on each request
+// server.express.use(async (req, res, next) => {
+//   // if they aren't logged in, skip this
+//   if (!req.userId) return next()
+//   const user = await db.query.user(
+//     { where: { id: req.userId } },
+//     "{ id, permissions, email, name }"
+//   )
+//   req.user = user
+//   next()
+// })
+
+// server.start(
+//   {
+//     cors: {
+//       credentials: true,
+//       origin: process.env.FRONTEND_URL,
+//     },
+//   },
+//   details => {
+//     console.log(`Server is now running on port http:/localhost:${details.port}`)
+//   }
+// )
