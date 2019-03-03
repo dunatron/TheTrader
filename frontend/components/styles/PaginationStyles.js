6a7fde30065f284942b9852e5767f5af82c17b25
Dunatron <heath.dunlop.hd@gmail.com>
2019-03-03 21:29:37 +1300

    ff

--- frontend/components/styles/PaginationStyles.js
+++ frontend/components/styles/PaginationStyles.js
@@ -1,12 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
 const PaginationStyles = styled.div`
   text-align: center;
-  display: inline-grid;
-  grid-template-columns: repeat(4, auto);
-  align-items: stretch;
-  justify-content: center;
-  align-content: center;
+
   margin: 2rem 0;
   border: 1px solid ${props => props.theme.lightgrey};
   border-radius: 10px;
@@ -18,10 +14,18 @@ const PaginationStyles = styled.div`
       border-right: 0;
     }
   }
-  a[aria-disabled='true'] {
+  a[aria-disabled="true"] {
     color: grey;
     pointer-events: none;
   }
-`;
+  @media (min-width: ${props => props.theme.breakpoints.values.md}px) {
+    /* margin: 60px 0 0 0; */
+    display: inline-grid;
+    grid-template-columns: repeat(4, auto);
+    align-items: stretch;
+    justify-content: center;
+    align-content: center;
+  }
+`
 
-export default PaginationStyles;
+export default PaginationStyles
